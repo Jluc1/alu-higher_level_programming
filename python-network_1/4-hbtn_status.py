c98f0fa50bf69b619ffa708c836c9021c9ffcba5
Jluc1 <j.habarugir@alustudent.com>
2023-08-01 12:37:22 +0000

    fixed typo in code

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ requests model
 
 if __name__ == '__main__':
     import requests
-    html = requests.get('https://alx-intranet.hbtn.io/status')
+    html = requests.get('https://alu-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(html.text.__class__))
     print("\t- content: {}".format(html.text))
